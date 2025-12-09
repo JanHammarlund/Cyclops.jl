bc5aa096ff0c2c6dfe080e95cc04db0a90d9ed19
JanHammarlund <hammarlund.jan@gmail.com>
2025-12-09 18:42:37 -0500

    train!
    updated input arguments and keyword arguments. no longer required to add the loss function as a positional argument. added cyclops.mse function to compute the reconstruction error. cyclops.mse is the new default loss function in the cyclops.train! function. also cleaned the output of the sample projections to circadian space. no longer broken down into out.c.i/f.x/y, but just out.c.i/f.

--- src/CyclopsOptimization.jl
+++ src/CyclopsOptimization.jl
@@ -1,9 +1,22 @@
-function train!(m::cyclops, l::Function, d::AbstractArray{T1};
+function mse(m::cyclops, x::AbstractVecOrMat{T}) where T <: AbstractFloat
+    return Flux.mse(m(x), x)
+end
+
+function train!(m::cyclops, d::AbstractArray{T1};
+    l::Function     = mse,
+    epochs::T2      = 100,
     steps::T2       = 100,
     batchsize::T2   = 1,
     shuffled::Bool  = true,
     optimiser::T3   = Adam()
     ) where {T1<:AbstractFloat, T2<:Integer, T3<:AbstractRule}
+
+    epoch_default = 100
+    step_default = 100
+
+    es = [epochs != epoch_default, steps != step_default]
+
+    epochs = any(es) ? [epochs, steps][findfirst(es)] : epochs
 
     initial_phases = decode(d, m)
     initial_projections = project(d, m)
@@ -14,7 +27,7 @@ function train!(m::cyclops, l::Function, d::AbstractArray{T1};
 
     mean_sample_loss = [mean([l(m, di) for di in eachcol(d)])]
 
-    @showprogress for _ in 1:steps
+    @showprogress for _ in 1:epochs
         Flux.train!(l, m, training_data, optimiser_rule)
         push!(mean_sample_loss, mean([l(m, di) for di in eachcol(d)]))
     end
@@ -24,7 +37,7 @@ function train!(m::cyclops, l::Function, d::AbstractArray{T1};
     final_transformation = m(d)
 
     phase_output = (i = initial_phases, f = final_phases)
-    projection_output = (i = (x = initial_projections[1, :], y = initial_projections[2, :]), f = (x = final_projections[1, :], y = final_projections[2, :]))
+    projection_output = (i = initial_projections, f = final_projections)
     transformation_output = (i = initial_transformation, f = final_transformation)
 
     output = (
